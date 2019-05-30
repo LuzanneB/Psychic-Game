2be60ffa92f5a4df7f596c387322d8681e6e9cf0
Luzanne Burt <luzannea@gmail.com>
2019-05-30 06:37:21 -0400

    prelim html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,42 @@
 // computer selects random letter
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var wins = 0;
+var losses = 0;
+var guessesleft = 9;
+
+
+var winsText=document.getElementById("wins");
+var lossesText=document.getElementById("losses");
+var guessesText=document.getElementById("guessesleft");
+var keyspressedText=document.getElementById("keyspressed");
+
+
 // onkey, computer checks to see if input matches selcted letter
-// if it does not match:
-    // updated guesses left to minus 1
-    // show letter guessed in "key pressed"
+document.onkeyup = function(event){
+
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 // if guess matches computer:
+if (userGuess === computerGuess){wins++;}
+else {losses++; guesses--}
+
     // update wins to +1
+winsText.textContent = wins;
     // reset guesses left to 9
     // reset keyspressed to empty
     // computer to select new random letter
+// if it does not match:
+    // updated guesses left to minus 1
+guessesText.textcontent = guessesleft;
+    // show letter guessed in "key pressed"
+
 // if guesses left reaches 0
     // update losses to +1
+lossesText.textContent = losses;
     // reset guesses left to 0
     // reset keys pressed to empty
     // computer to select new random letter
-    
+
+}
