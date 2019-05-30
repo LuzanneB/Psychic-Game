671d85c38f46bb7ed82dbe32c3b0144b94df2518
Luzanne Burt <luzannea@gmail.com>
2019-05-29 20:19:11 -0400

    pseudo code js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// computer selects random letter
+// onkey, computer checks to see if input matches selcted letter
+// if it does not match:
+    // updated guesses left to minus 1
+    // show letter guessed in "key pressed"
+// if guess matches computer:
+    // update wins to +1
+    // reset guesses left to 9
+    // reset keyspressed to empty
+    // computer to select new random letter
+// if guesses left reaches 0
+    // update losses to +1
+    // reset guesses left to 0
+    // reset keys pressed to empty
+    // computer to select new random letter
+    
