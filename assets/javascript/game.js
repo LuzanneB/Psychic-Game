f14bb270bc4a64e9df73a0c968bf1fa5edef2e46
Luzanne Burt <luzannea@gmail.com>
2019-05-31 09:12:27 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,58 +4,57 @@ var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","
 var wins = 0;
 var losses = 0;
 var guessesleft = 9;
+var guessSoFar=[];
+var computerGuess = ""
 
 
-var winsText=document.getElementById("wins");
-var lossesText=document.getElementById("losses");
-var guessesText=document.getElementById("guessesleft");
-var keyspressedText=document.getElementById("keyspressed");
+// var winsText=document.getElementById("wins");
+// var lossesText=document.getElementById("losses");
+// var guessesText=document.getElementById("guessesleft");
+// var keyspressedText=document.getElementById("keyspressed");
 
 
 // onkey, computer checks to see if input matches selcted letter
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log("the computer picked "+ computerGuess);
+function randomselection(){
+ computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log("the computer picked "+ computerGuess);}
+
+function updateScores(){
+        document.querySelector("#wins").innerHTML = wins;
+        document.querySelector("#losses").innerHTML = losses;
+        document.querySelector("#guessesleft").innerHTML = guessesleft;
+        document.querySelector("#keyspressed").innerHTML = guessSoFar;
+        }
+
+function resetScores(){
+        guessesleft = 9;
+        guessSoFar = [];
+}
+
+randomselection();
+
+
 
 document.onkeyup = function(event){ 
+        guessesleft --;
 
-    var userGuess = event.key.toLowerCase();
-    console.log(event);
+        var userGuess = event.key.toLowerCase();
+                console.log(event);
+        
+        guessSoFar.push(userGuess);
 
+        updateScores();
 
-// // if guess matches computer:
-//     // update wins to +1
-//     // reset guesses left to 9
-//     // reset keyspressed to empty
-if (userGuess == computerGuess){
-        wins++;
-        winsText.textContent= wins;
-        guessesText.textContent=9;
-        keyspressedText.textContent="-"}
-        // // if guesses left reaches 0
-//     // update losses to +1
-//     // reset guesses left to 9
-else if (guessesleft = 0){
-        losses++;
-        lossesText.textContent = losses;
-        guessesText.textContent=9;
-        keyspressedText.textContent="-"}
-else{
-    guessesleft--;
-
-}};
-
-console.log(wins);
-console.log(guessesleft);
-
-
-
-//     // computer to select new random letter
-// // if it does not match:
-//     // updated guesses left to minus 1
-guessesText.textcontent = guessesleft;
-//     // show letter guessed in "key pressed"
-
-
-//     // reset keys pressed to empty
-//     // computer to select new random letter
-
+        if (userGuess === computerGuess){
+                wins++;
+                updateScores();
+                resetScores();
+                randomselection();
+        }
+        if(guessesleft===0){
+                losses++;
+                updateScores();
+                resetScores();
+                randomselection();
+        }
+}
